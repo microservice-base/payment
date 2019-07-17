75a542a1136f03249a219530748605e8beddebe5
kerami <keramiozsoy@hotmail.com>
2019-07-17 21:53:38 +0300

    home method

--- payment/paymentApplication.py
+++ payment/paymentApplication.py
@@ -4,11 +4,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class Home(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(Home, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=8004)
