7061b55e78acc32c30382b8b48aad6a24b9485fe
kerami <keramiozsoy@hotmail.com>
2019-07-19 23:51:10 +0300

    simple python

--- payment/paymentApplication.py
+++ payment/paymentApplication.py
@@ -1,11 +1,17 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    json = ['test1','test2','test3']
+    json = ['test1', 'test2', 'test3']
     return jsonify(json)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'This page does not exist', 404
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8004)
